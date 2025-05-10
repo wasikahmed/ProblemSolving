d972f49d2668e972d34a462cc8710f6c4e9a8e6c
M R Wasik Ahmed Apon <aponwasikahmed@gmail.com>
2025-05-10 16:20:56 +0600

    Update solution for LeetCode prob 500(Keyboard Row)

--- LeetCode/500.py
+++ LeetCode/500.py
@@ -5,29 +5,39 @@ from typing import List
 
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
-        row1, row2, row3 = "qwertyuiop", "asdfghjkl", "zxcvbnm"
-        res = []
+        # row1, row2, row3 = "qwertyuiop", "asdfghjkl", "zxcvbnm"
+        # res = []
+        #
+        # for i in range(len(words)):
+        #     word = words[i].lower()
+        #     row = None
+        #     flag = True
+        #     if word[0] in row1:
+        #         row = row1
+        #     elif word[0] in row2:
+        #         row = row2
+        #     else:
+        #         row = row3
+        #
+        #     for j in range(1, len(word)):
+        #         if word[j] not in row:
+        #             flag = False
+        #             break
+        #
+        #     if flag:
+        #         res.append(words[i])
+        #
+        # return res
 
-        for i in range(len(words)):
-            word = words[i].lower()
-            row = None
-            flag = True
-            if word[0] in row1:
-                row = row1
-            elif word[0] in row2:
-                row = row2
-            else:
-                row = row3
+        rows = [set("qwertyuiop"), set("asdfghjkl"), set("zxcvbnm")]
 
-            for j in range(1, len(word)):
-                if word[j] not in row:
-                    flag = False
-                    break
+        def is_one_row(word: str) -> bool:
+            for row in rows:
+                if word[0].lower() in row:
+                    return all(c.lower() in row for c in word)
+            return False
 
-            if flag:
-                res.append(words[i])
-
-        return res
+        return [word for word in words if is_one_row(word)]
 
 
 def main():
